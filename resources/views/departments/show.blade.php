ba8b8589967c17376e63d6cc72c0a39e49f67a12
itsmedaveee <agentace.xx@gmail.com>
2022-08-18 20:31:18 +0800

    fix

--- resources/views/departments/show.blade.php
+++ resources/views/departments/show.blade.php
@@ -55,32 +55,7 @@
 				</div>
 			</div>
 
-			<div class="col-md-12">
-				<div class="panel panel-default">
-				<div class="panel-heading">Faculties Lists</div>
-				<div class="panel-body">
-					<table class="table table-bordered">
-						<thead>
-							<tr>
-								<th>ID No.</th>
-								<th>Fullname</th>
-								<th>Gender</th>
-							</tr>
-						</thead>
-						<tbody>
-							@foreach ($department->faculties as $faculty)
-							<tr>
-								<td>{{ $faculty->id_number }}</td>
-								<td>{{ $faculty->name }}</td>
-								<td>{{ $faculty->gender }}</td>
-							</tr>
-							@endforeach
-						</tbody>
-					</table>
-					</div>
-				</div>
-		</div>
-	</div>
+		 
 
 
 
