abb9ceace1681ac12ea8615dcebbd5f6aa8b8601
itsmedaveee <agentace.xx@gmail.com>
2022-08-29 22:53:32 +0800

    added jquery

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -14,6 +14,45 @@
 
 	@yield ('content')
 
+
+	
+
+
+    <script src="{{ asset('/js/toastr.min.js') }}"></script>
+        <script src="{{ asset('/js/app.min.js') }}"></script>
+     <script type="text/javascript">
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': "{{ csrf_token() }}"
+        }
+    });
+    </script>
+
+    @stack ('scripts')
+
+
+
+    <script>
+   
+        @if(Session::has('success'))
+            toastr.success("{{ Session::get('success') }}");
+            {{ session()->forget('success') }}
+        @endif
+
+        @if(Session::has('info'))
+        toastr.info("{{ Session::get('info') }}");
+        @endif
+
+        @if(Session::has('warning'))
+        toastr.warning("{{ Session::get('warning') }}");
+        @endif
+
+        @if(Session::has('error'))
+            toastr.error("{{ Session::get('error') }}");
+        @endif
+     
+    </script>
+
 	
 </body>
 </html>
